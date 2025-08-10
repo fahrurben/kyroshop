f92659fa433ef9cb48f74d1c51222295852f18d4
fahrur <fahrurben@gmail.com>
2025-08-10 07:34:10 +0700

    Fix auth token removed when refresh

--- kyroshop-frontend/src/hooks/use-auth.js
+++ kyroshop-frontend/src/hooks/use-auth.js
@@ -2,13 +2,15 @@ import { useSnapshot } from 'valtio'
 import { store as authStore } from '../stores/auth.store.js'
 import { useEffect } from 'react'
 import { useNavigate } from 'react-router'
+import axios from 'axios'
 
 export default function useAuth () {
-  const { authExpiredAt } = useSnapshot(authStore)
+  const { authExpiredAt, authToken } = useSnapshot(authStore)
   const navigate = useNavigate()
 
   useEffect(() => {
-    let authTokenExpiryTime = new Date(authExpiredAt*1000);
+    let authTokenExpiryTime = new Date(authExpiredAt*1000)
+    axios.defaults.headers.common["Authorization"] = "Bearer " + authToken
     let now = new Date()
     if (now > authTokenExpiryTime) {
       navigate('/login')
